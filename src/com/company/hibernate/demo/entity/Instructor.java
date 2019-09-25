300cb51d703eac12249dec86362df0e080d4d345
Nnamdi <nwabuokeinnamdi@gmail.com>
2019-09-26 00:27:12 +0100

    Create a review table and mapped the fields to their respective columns

--- src/com/company/hibernate/demo/entity/Instructor.java
+++ src/com/company/hibernate/demo/entity/Instructor.java
@@ -34,7 +34,7 @@ public class Instructor {
     
     //Resolving lazy loading issues and still access the data
     //Option 1: Call the getter method while the session is open
-    //Option 2: Q
+    //Option 2: HQL Query
     @OneToMany(fetch = FetchType.LAZY,
             mappedBy = "instructor", cascade = {CascadeType.DETACH,
             CascadeType.MERGE,
