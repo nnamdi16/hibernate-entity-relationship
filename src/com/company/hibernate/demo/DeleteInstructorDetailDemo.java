d3f475804b1f4a16506f405d48f51cabe9db6aa2
Nnamdi <nwabuokeinnamdi@gmail.com>
2019-09-25 08:41:20 +0100

    Modify the CascadeType in InstructorDetail.java so that deleting an instructorDetail doesn't make for deleting an instructor.

---
+++ src/com/company/hibernate/demo/DeleteInstructorDetailDemo.java
@@ -0,0 +1,53 @@
+package com.company.hibernate.demo;
+
+import com.company.hibernate.demo.entity.Instructor;
+import com.company.hibernate.demo.entity.InstructorDetail;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+public class DeleteInstructorDetailDemo {
+
+    public static void main(String[] args) {
+        //Create session factory
+        SessionFactory factory = new Configuration()
+                .configure("hibernate.cfg.xml")
+                .addAnnotatedClass(Instructor.class)
+                .addAnnotatedClass(InstructorDetail.class)
+                .buildSessionFactory();
+
+        //Create session
+        Session session = factory.getCurrentSession();
+
+        try {
+            //Use the session object to save Java Object
+
+
+            //Start a transaction
+            session.beginTransaction();
+            //Get the instructor detail
+            int theId = 3;
+            InstructorDetail tempInstructorDetail = session.get(InstructorDetail.class, theId);
+
+            //Print the instructor detail
+            System.out.println("tempInstructorDetail: " + tempInstructorDetail);
+
+            //Print the associated Instructor
+            System.out.println("the associated instructor: " + tempInstructorDetail.getInstructor());
+
+            //Delete the instructor detail
+            System.out.println("Deleting tempInstructor");
+            session.delete(tempInstructorDetail);
+
+            session.getTransaction().commit();
+            System.out.println("Done !!");
+        } catch (Exception exc) {
+            exc.printStackTrace();
+        } finally {
+            //handle connection leak issue
+            session.close();
+
+            factory.close();
+        }
+    }
+}
--- src/com/company/hibernate/demo/entity/InstructorDetail.java
+++ src/com/company/hibernate/demo/entity/InstructorDetail.java
@@ -21,7 +21,8 @@ public class InstructorDetail {
 
     //Add new field for instructor (also add getters/setters)
     //Add OneToOne annotation (bi-directional)
-    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+//    @OneToOne(mappedBy = "instructorDetail", cascade = CascadeType.ALL)
+    @OneToOne(mappedBy = "instructorDetail", cascade = {CascadeType.DETACH, CascadeType.MERGE, CascadeType.PERSIST, CascadeType.REFRESH})
     private Instructor instructor;
 
     public InstructorDetail() {
